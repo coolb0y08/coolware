e1c60f897519075850491fe486434a09bbd6154b
coolb0y08 <93867745+coolb0y08@users.noreply.github.com>
2021-11-07 16:03:39 +0200

    Update ui.lua

--- etc/ui.lua
+++ etc/ui.lua
@@ -1,8 +1,2 @@
 -- Yo.
-local __1, __2 = pcall(function()
-  return loadstring(game:HttpGet("https://raw.githubusercontent.com/CatzCode/cattoware/main/libraries/ui.lua"))()
-end)
-
-if not __1 then
-  game.Players.LocalPlayer:Kick("[Failed to load cattoware/main/libraries/ui.lua]\n\n"..__2)
-end;
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/CatzCode/cattoware/main/libraries/ui.lua"))()
