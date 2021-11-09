0bca998269b90cdaac828504c7694871c37e24b7
coolb0y08 <93867745+coolb0y08@users.noreply.github.com>
2021-11-09 18:19:50 +0200

    Update ui.lua

--- etc/ui.lua
+++ etc/ui.lua
@@ -1,9 +1,1 @@
-local library = {}
-
-library.load(str)
-  if str == "cattoware" then
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/CatzCode/cattoware/main/libraries/ui.lua"))()
-  elseif str == "uwuware then
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/un-named%20backups/jans"))()
-  end
-end
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/CatzCode/cattoware/main/libraries/ui.lua"))()
